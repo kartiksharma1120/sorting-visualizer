44c871f1c52e3c109f938176552510e3db3d44ab
kartiksharma1120 <125125893+kartiksharma1120@users.noreply.github.com>
2023-09-23 18:11:32 +0530

    files

---
+++ javascript_files/bubble.js
@@ -0,0 +1,34 @@
+async function bubble() {
+    console.log('In bubbe()');
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length-1; i++){
+        console.log('In ith loop');
+        for(let j = 0; j < ele.length-i-1; j++){
+            console.log('In jth loop');
+            ele[j].style.background = 'blue';
+            ele[j+1].style.background = 'blue';
+            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
+                console.log('In if condition');
+                await waitforme(delay);
+                swap(ele[j], ele[j+1]);
+            }
+            ele[j].style.background = '#ff2963';
+            ele[j+1].style.background = '#ff2963';
+        }
+        ele[ele.length-1-i].style.background = 'green';
+        playDoneSound();
+    }
+    ele[0].style.background = 'green';
+    playCompleteSound();
+}
+
+const bubSortbtn = document.querySelector(".bubbleSort");
+bubSortbtn.addEventListener('click', async function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await bubble();
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+});
